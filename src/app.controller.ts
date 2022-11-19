951947925229adf57d94bcc831c712c5d57f5073
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-11-19 10:32:03 +0000

    server start

---
+++ src/app.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('/api')
+export class AppController {
+  constructor(private appService: AppService) {}
+  @Get()
+  getUser() {
+    return this.appService.getUsers();
+  }
+}
